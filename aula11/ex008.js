a597f4ede6170e4a042b34ff37ec50f1d52b8aa8
jatasas <jv202449@gmail.com>
2024-12-03 13:39:52 -0400

    curso de js

---
+++ aula11/ex008.js
@@ -0,0 +1,9 @@
+var vel = 59
+
+console.log(`a velocidade do seu carro e ${vel} km/h`)
+
+if (vel > 60) {  //consicao simples!
+    console.log(`voce ultrapassou a velocidade permitida. MULTADO`)
+}
+
+console.log(`dirija sempre usando cinto de seguranca`)
---
+++ aula11/ex009.js
@@ -0,0 +1,9 @@
+var pais = 'brasil'
+
+console.log(`vivendo em ${pais}`)
+
+if (pais == 'brasil') {
+    console.log(`voce e brasileiro`)
+} else {
+    console.log(`voce e estrangeiro`)
+}
